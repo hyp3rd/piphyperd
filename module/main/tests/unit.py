78b4384a16e66563842c47001cfd5c8865b20a51
Francesco Cosentino <fc@hyperd.sh>
2020-02-03 20:43:52 +0200

    pipeline: coverage fixes

--- module/main/tests/unit.py
+++ module/main/tests/unit.py
@@ -1,22 +1,42 @@
+"""
+THIS SOFTWARE IS PROVIDED AS IS
+and under GNU General Public License. <https://www.gnu.org/licenses/gpl-3.0.en.html>
+USE IT AT YOUR OWN RISK.
+
+PipHyperd unit testing.
+
+The module is published on PyPi <https://pypi.org/project/piphyperd/>.
+
+The code is available on GitLab <https://gitlab.com/hyperd/piphyperd>.
+"""
+
 import unittest
 import sys
 import os
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if not path in sys.path:
-    sys.path.insert(1, path)
-del path
 
-try:
+PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if not PATH in sys.path:
+    sys.path.insert(1, PATH)
     from piphyperd import PipHyperd
-except Exception as module_not_found:
-    print("No module piphyperd found")
+del PATH
 
 
 class TestMethods(unittest.TestCase):
+
+    """
+    PipHyperd unit testing class
+    """
+
     def setUp(self):
+        """
+        tests setup
+        """
         self.piphyperd = PipHyperd()
 
     def test_add(self):
+        """
+        Assert that PipHyperd is not None
+        """
         self.assertIsNotNone(self.piphyperd)
 
 
